b3e0fbb388f56ffc99f1b899a01af1f3d5988119
atif93 <atifahmed150893@gmail.com>
2016-11-14 11:33:56 -0500

    bfs cpp code

--- bfs_shortest_path.cpp
+++ bfs_shortest_path.cpp
@@ -3,87 +3,99 @@
     https://www.hackerrank.com/challenges/bfsshortreach
 */
 
-#include <stdio.h>
+#include <iostream>
+#include <queue>
+#include <list>
 
-int queue[1000], tail = -1, head = 0;
+using namespace std;
 
-int isQueueEmpty() {
-    if(tail<head) {
-        return 1;
-    }
-    return 0;
+class Graph {
+    int nVertices; // number of vertices in the graph
+    list<int> * adj; // adjacency list
+public:
+    Graph(int _nVertices); // constructor
+    void add_edge(int node1, int node2); // adding edge to the adjacency list
+    void breadth_first_search(int startingNode); // main logic for bfs 
+    void print_result(int * distance); // prints the bfs shortest reach results
+};
+
+Graph::Graph(int _nVertices) {
+    nVertices = _nVertices;
+    adj = new list<int>[nVertices];
 }
 
-void pushQueue(int s) {
-    queue[++tail] = s; 
+void Graph::add_edge(int node1, int node2) {
+    adj[node1].push_back(node2);
+    adj[node2].push_back(node1);
 }
 
-int popQueue() {
-   if(isQueueEmpty()) {
-       return -1;
-   } 
-   return queue[head++];
-}
-    
-int main() {
-    int q, n, m, s;
-    int i, j, node1, node2; 
-    int a[1000][1000], visited[1000], distance[1000];
-    
-    scanf("%d", &q);
-    while(q--) {
-        scanf("%d %d", &n, &m);
-        for(i=0; i<n; i++) {
-            for(j=0; j<=i; j++) {
-                a[i][j] = 0;
-                a[j][i] = 0;
+void Graph::breadth_first_search(int startingNode) {
+    bool * visited = new bool[nVertices](); // intialized to 0 with brackets at the end
+    //int * distance = new int[nVertices]{-1}; // intialized to -1 // can be used with --std=c++0x flag (C++11)
+    int * distance = new int[nVertices]; 
+    std::fill_n(distance, nVertices, -1); // intialized to -1
+    queue<int> mQueue;
+
+    mQueue.push(startingNode);
+    distance[startingNode] = 0;
+ 
+    // since all the edges are of equal length = 6
+    // we can just find the number of edges from the origin and multiply by 6
+    while(!mQueue.empty()) {
+        int s = mQueue.front();
+        mQueue.pop();
+        visited[s] = 1;
+        for(list<int>::iterator i = adj[s].begin(); i != adj[s].end(); i++) {
+            if(!visited[*i]) {
+                distance[*i] = distance[s] + 1;
+                visited[*i] = 1;
+                mQueue.push(*i);
             }
         }
-        for(i=0; i<m; i++) {
-            scanf("%d %d", &node1, &node2);
+    }   
+
+    print_result(distance);
+
+    delete [] visited;
+    delete [] distance;
+}
+
+void Graph::print_result(int * distance) {
+    for(int i=0; i<nVertices; i++) {
+        if(distance[i] != 0) {
+            if(distance[i] != -1) {
+                cout << 6 * distance[i]);
+            } else {
+                cout << distance[i]);                 
+            }
+        }
+    }
+    cout << endl;
+}
+
+int main() {
+    int q, n, m, s;
+    int node1, node2; 
+    
+    cin >> q;
+    while(q--) {
+        cin >> n >> m;
+        
+        Graph graph(n);
+
+        for(int i=0; i<m; i++) {
+            cin >> node1 >> node2;
             // since the indices are starting from 0
             node1--; 
             node2--;
-            a[node1][node2] = 6;
-            a[node2][node1] = 6;
+            graph.add_edge(node1, node2);
+            graph.add_edge(node2, node1);
         }
-        scanf("%d", &s);
+        cin >> s;
         // since the indices are starting from 0
         s--;
-        head = 0;
-        tail = -1;
-        pushQueue(s);
         
-        for(i=0; i<n; i++) {
-            visited[i] = 0;
-            distance[i] = -1;
-        }
-        distance[s] = 0;
-        
-        // since all the edges are of equal length = 6
-        // we can just find the number of edges from the origin and multiply by 6
-        while(!isQueueEmpty()) {
-            s = popQueue();
-            visited[s] = 1;
-            for(i=0; i<n; i++) {
-                if(a[s][i] == 6 && visited[i] == 0) {
-                    distance[i] = distance[s] + 1;
-                    visited[i] = 1;
-                    pushQueue(i);
-                }
-            }
-        }
-      
-        for(i=0; i<n; i++) {
-            if(distance[i] != 0) {
-                if(distance[i] != -1) {
-                    printf("%d ", 6 * distance[i]);
-                } else {
-                    printf("%d ", distance[i]);                 
-                }
-            }
-        }
-        printf("\n");
+        graph.breadth_first_search(s);
     }
     
     return 0;
